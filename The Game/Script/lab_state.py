faaa397ce7bb987bfd3df25bbc488d86a24384b4
khybin <134588918+khybin@users.noreply.github.com>
2023-08-30 20:32:43 +0900

    s

--- The Game/Script/lab_state.py
+++ The Game/Script/lab_state.py
@@ -29,6 +29,7 @@ class LabState:
         self.show_lab_researcher_text = False
         self.one_time = True
         self.keydoor_flag = False
+        self.keykard_flag = True
         # lab_switch를 눌렀을 때 텍스트를 표시하는 플래그
         self.show_text = False
         self.noclick = False
@@ -43,6 +44,7 @@ class LabState:
         self.inventory_wire = pygame.image.load(self.inventory_wire_path)
         self.keykard_path = os.path.join(self.image_directory, "lab/keykard.png")
         self.keykard = pygame.image.load(self.keykard_path)
+        
         
         # ... (다른 이미지들 로드 및 Rect 설정)
         self.lab_clock_path = os.path.join(self.image_directory, "lab/lab_clock.png")
@@ -79,27 +81,65 @@ class LabState:
         self.inventory_visible = False
         self.other_interaction_state = False
 
+        self.slot_clicked = False  # 슬롯이 클릭되었는지 여부를 나타내는 변수
+
+        # 격자 정보
+        self.num_rows = 4
+        self.num_cols = 4
+        self.slot_size = 100  # 슬롯 크기를 늘림
+        self.slot_margin = 10
+        self.inventory_width = self.num_cols * (self.slot_size + self.slot_margin)
+        self.inventory_height = self.num_rows * (self.slot_size + self.slot_margin)
+
+        self.inventory_x = (self.screen_width - self.inventory_width) // 2
+        self.inventory_y = (self.screen_height - self.inventory_height) // 2
+
+        self.black = (0, 0, 0)
+        self.white = (255, 255, 255)
+        self.gray = (200, 200, 200)
+
+        self.inventory_equipped_item = None
+        self.equipped_item = None  # 현재 장착된 아이템을 저장하는 변수
+
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             if self.inventory == "inventory" or self.noclick:
-                pass
+                for row in range(self.num_rows):
+                    for col in range(self.num_cols):
+                        slot_x = self.inventory_x + col * (self.slot_size + self.slot_margin)
+                        slot_y = self.inventory_y + row * (self.slot_size + self.slot_margin)
+                        slot_rect = pygame.Rect(slot_x, slot_y, self.slot_size, self.slot_size)
+                        
+                        if slot_rect.collidepoint(event.pos):
+                                self.slot_clicked = True  # 슬롯 클릭되었음을 표시
+                        if self.slot_clicked:
+                            for row in range(self.num_rows):
+                                for col in range(self.num_cols):
+                                    slot_x = self.inventory_x + col * (self.slot_size + self.slot_margin)
+                                    slot_y = self.inventory_y + row * (self.slot_size + self.slot_margin)
+                                    slot_rect = pygame.Rect(slot_x, slot_y, self.slot_size, self.slot_size)
+
+                                    if slot_rect.collidepoint(event.pos):
+                                        clicked_item_index = col + row * self.num_cols
+                                        if clicked_item_index < len(self.inventory_items):
+                                            self.equipped_item = self.inventory_items[clicked_item_index]  # 클릭한 아이템을 장착
+                                            self.slot_clicked = False  # 슬롯 클릭 해제
             else:
                 if self.lab_clock_rect.collidepoint(event.pos):
                     print("시계다")
                 elif self.lab_computer_rect.collidepoint(event.pos):
                     self.lab_computer_flag = True
                 elif self.lab_door_rect.collidepoint(event.pos):
-                    #self.game_state = "hollway"
-                    if self.inventory_equipped_item == "keykard":
-                        self.show_lab_door_text = True
-                        self.keydoor_flag = True
-                    else:    
-                        self.show_lab_door_text = True
+                    self.game_state = "hollway"  
+                        # self.show_lab_door_text = True
+                        # self.keydoor_flag = True
 
                 elif self.lab_researcher_rect.collidepoint(event.pos):
                     #print("시체다.")
+                    if self.keykard_flag:
+                        self.inventory_items.append(self.keykard)
+                        self.keykard_flag = False
                     self.show_lab_researcher_text = True
-                    self.inventory_items.append(self.keykard)
                 elif self.lab_profile_rect.collidepoint(event.pos):
                     self.lab_profile_flag = False
                     self.show_lab_profile_text = True
@@ -127,6 +167,12 @@ class LabState:
                         # 이미지가 보여지고, 텍스트 창이 숨겨진 경우에는 "z" 키로 이미지를 지웁니다.
                         self.lab_profile_flag = True
                         self.noclick = False
+                elif self.show_lab_researcher_text and self.one_time:
+                        self.text_start_time = None  # 텍스트 시작 시간 초기화
+                        self.show_text = False
+                        self.one_time = False
+                        self.show_lab_researcher_text = False
+                        print('1')
                 else :
                         self.show_lab_switch_text = False
                         self.show_lab_wire_text = False
@@ -134,11 +180,6 @@ class LabState:
                         self.show_text = False
                         self.show_lab_door_text = False
                         self.show_lab_researcher_text = False
-                if self.one_time:
-                        self.text_start_time = None
-                        self.show_text = False
-                        self.show_lab_researcher_text = False
-                        self.one_time = False
                 if self.keydoor_flag:
                         self.text_start_time = None  # 텍스트 시작 시간 초기화
                         self.show_text = False
@@ -161,6 +202,46 @@ class LabState:
 
     def get_inventory_items(self):
         return self.inventory_items
+    
+    def draw_inventory(self):
+        inventory_x = (self.screen_width - self.inventory_width) // 2
+        inventory_y = (self.screen_height - self.inventory_height) // 2
+
+        # 인벤토리 창 배경 그리기 (크기 조정 및 모서리 둥글게 처리)
+        border_radius = 20  # 모서리의 둥글기 정도 설정
+        pygame.draw.rect(self.screen, self.black, (inventory_x-10, inventory_y-10, self.inventory_width+10, self.inventory_height+10), border_radius=border_radius)
+
+        slot_index = 0  # 슬롯의 인덱스 초기화
+        for row in range(self.num_rows):
+            for col in range(self.num_cols):
+                slot_x = inventory_x + col * (self.slot_size + self.slot_margin)
+                slot_y = inventory_y + row * (self.slot_size + self.slot_margin)
+                pygame.draw.rect(self.screen, self.gray, (slot_x, slot_y, self.slot_size, self.slot_size))
+
+                # 인벤토리 아이템 텍스트를 그리기
+                item_index = col + row * self.num_cols
+                if item_index < len(self.inventory_items):
+                    item_image = self.inventory_items[item_index]
+
+                    item_image_rect = item_image.get_rect()
+                    item_image_rect.center = (slot_x + self.slot_size // 2, slot_y + self.slot_size // 2)
+                    self.screen.blit(item_image, item_image_rect)
+
+        for row in range(self.num_rows):
+            for col in range(self.num_cols):
+                slot_x = inventory_x + col * (self.slot_size + self.slot_margin)
+                slot_y = inventory_y + row * (self.slot_size + self.slot_margin)
+                slot_rect = pygame.Rect(slot_x, slot_y, self.slot_size, self.slot_size)
+
+                pygame.draw.rect(self.screen, self.gray, slot_rect)
+
+                item_index = col + row * self.num_cols
+                if item_index < len(self.inventory_items):
+                    item_image = self.inventory_items[item_index]
+                    item_image_rect = item_image.get_rect(center=slot_rect.center)
+                    self.screen.blit(item_image, item_image_rect)
+
+                slot_index += 1  # 슬롯 인덱스 증가
 
     def draw(self, screen):
         screen.blit(self.lab_clock, self.lab_clock_rect)
@@ -203,13 +284,6 @@ class LabState:
                 self.text_start_time = pygame.time.get_ticks()
             self.elapsed_time = pygame.time.get_ticks() - self.text_start_time
             self.show_text_box("문이 잠겨 있다.", self.elapsed_time)
-        elif self.show_lab_door_text and self.keydoor_flag:
-            self.show_text = True
-            if self.text_start_time is None:
-                self.text_start_time = pygame.time.get_ticks()
-            self.elapsed_time = pygame.time.get_ticks() - self.text_start_time
-            self.show_text_box("ID카드가 망가졌는지 열리지 않는다.", self.elapsed_time)
-            pygame.display.flip()
         if self.show_lab_researcher_text and self.one_time:
             self.show_text = True
             if self.text_start_time is None:
@@ -224,3 +298,36 @@ class LabState:
             self.elapsed_time = pygame.time.get_ticks() - self.text_start_time
             self.show_text_box("뒷면에는 Dawoo라는 글자와 0412이라는 숫자가 적혀 있다.", self.elapsed_time)
             pygame.display.flip()
+
+        if self.inventory == "inventory":
+            self.draw_inventory()  # 인벤토리 창 그리기
+
+        equipped_item_rect = pygame.Rect(self.screen_width - 110, self.screen_height - 110, 100, 100)
+
+        if self.equipped_item is not None:
+            equipped_item_rect = pygame.Rect(self.screen_width - 110, self.screen_height - 110, 100, 100)
+            equipped_item_image_rect = self.equipped_item.get_rect(center=equipped_item_rect.center)
+            box_size = 100
+            box_surface = pygame.Surface((box_size, box_size), pygame.SRCALPHA)  # 투명한 Surface 생성
+            pygame.draw.rect(box_surface, (0, 0, 0), pygame.Rect(0, 0, box_size, box_size), 3)  # 테두리 그리기
+
+            # 박스 배경을 투명하게 설정하고 장착된 아이템 이미지 그리기
+            screen.blit(box_surface, (equipped_item_rect.left - (box_size - equipped_item_rect.width) / 2,
+                                equipped_item_rect.top - (box_size - equipped_item_rect.height) / 2))
+            screen.blit(self.equipped_item, equipped_item_image_rect)  # 아이템 이미지 표시
+
+            # 아이템 종류에 따라 inventory_equipped_item 설정
+            if self.equipped_item == self.inventory_wire:
+                self.inventory_equipped_item = "lab_wire"
+            elif self.equipped_item == self.inventory_switch:
+                self.inventory_equipped_item = "lab_switch"
+            elif self.equipped_item == self.keykard:
+                self.inventory_equipped_item = "keykard"
+
+            if self.inventory_equipped_item == "keykard" and self.keydoor_flag:
+                self.show_text = True
+                if self.text_start_time is None:
+                    self.text_start_time = pygame.time.get_ticks()
+                self.elapsed_time = pygame.time.get_ticks() - self.text_start_time
+                self.show_text_box("ID카드가 망가졌는지 열리지 않는다.", self.elapsed_time)
+                pygame.display.flip()
--- The Game/Script/main.py
+++ The Game/Script/main.py
@@ -31,47 +31,6 @@ inventory_y = (screen_height - inventory_height) // 2
 
 inventory_equipped_item = None
 
-def draw_inventory():
-    inventory_x = (screen_width - inventory_width) // 2
-    inventory_y = (screen_height - inventory_height) // 2
-
-    # 인벤토리 창 배경 그리기 (크기 조정 및 모서리 둥글게 처리)
-    border_radius = 20  # 모서리의 둥글기 정도 설정
-    pygame.draw.rect(screen, black, (inventory_x-10, inventory_y-10, inventory_width+10, inventory_height+10), border_radius=border_radius)
-
-    slot_index = 0  # 슬롯의 인덱스 초기화
-    for row in range(num_rows):
-        for col in range(num_cols):
-            slot_x = inventory_x + col * (slot_size + slot_margin)
-            slot_y = inventory_y + row * (slot_size + slot_margin)
-            pygame.draw.rect(screen, gray, (slot_x, slot_y, slot_size, slot_size))
-
-            # 인벤토리 아이템 텍스트를 그리기
-            item_index = col + row * num_cols
-            if item_index < len(inventory_items):
-                item_image = inventory_items[item_index]
-
-                item_image_rect = item_image.get_rect()
-                item_image_rect.center = (slot_x + slot_size // 2, slot_y + slot_size // 2)
-                screen.blit(item_image, item_image_rect)
-
-    for row in range(num_rows):
-        for col in range(num_cols):
-            slot_x = inventory_x + col * (slot_size + slot_margin)
-            slot_y = inventory_y + row * (slot_size + slot_margin)
-            slot_rect = pygame.Rect(slot_x, slot_y, slot_size, slot_size)
-
-            pygame.draw.rect(screen, gray, slot_rect)
-
-            item_index = col + row * num_cols
-            if item_index < len(inventory_items):
-                item_image = inventory_items[item_index]
-                item_image_rect = item_image.get_rect(center=slot_rect.center)
-                screen.blit(item_image, item_image_rect)
-
-            slot_index += 1  # 슬롯 인덱스 증가
-
-
 # 색상 정의
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -117,18 +76,6 @@ while running:
             inventory = hollway_state.inventory
             hollway_state.handle_event(event)
             game_state = hollway_state.game_state
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if inventory == "inventory":
-                for row in range(num_rows):
-                    for col in range(num_cols):
-                        slot_x = inventory_x + col * (slot_size + slot_margin)
-                        slot_y = inventory_y + row * (slot_size + slot_margin)
-                        slot_rect = pygame.Rect(slot_x, slot_y, slot_size, slot_size)
-                        
-                        if slot_rect.collidepoint(event.pos):
-                                slot_clicked = True  # 슬롯 클릭되었음을 표시
-                        
-                        
 
     screen.fill(white)
 
@@ -138,47 +85,6 @@ while running:
         lab_state.draw(screen)
     elif game_state == "hollway":
         hollway_state.draw(screen)
-
-    if inventory == "inventory":
-        draw_inventory()  # 인벤토리 창 그리기
-
-    if slot_clicked:
-        for row in range(num_rows):
-            for col in range(num_cols):
-                slot_x = inventory_x + col * (slot_size + slot_margin)
-                slot_y = inventory_y + row * (slot_size + slot_margin)
-                slot_rect = pygame.Rect(slot_x, slot_y, slot_size, slot_size)
-
-                if slot_rect.collidepoint(event.pos):
-                    clicked_item_index = col + row * num_cols
-                    if clicked_item_index < len(inventory_items):
-                        equipped_item = inventory_items.pop(clicked_item_index)  # 클릭한 아이템을 장착하고 목록에서 제거
-                        slot_clicked = False  # 슬롯 클릭 해제
-
-
-    equipped_item_rect = pygame.Rect(screen_width - 110, screen_height - 110, 100, 100)
-
-    if equipped_item is not None:
-        equipped_item_rect = pygame.Rect(screen_width - 110, screen_height - 110, 100, 100)
-        equipped_item_image_rect = equipped_item.get_rect(center=equipped_item_rect.center)
-        box_size = 100
-        box_surface = pygame.Surface((box_size, box_size), pygame.SRCALPHA)  # 투명한 Surface 생성
-        pygame.draw.rect(box_surface, (0, 0, 0), pygame.Rect(0, 0, box_size, box_size), 3)  # 테두리 그리기
-
-        # 박스 배경을 투명하게 설정하고 장착된 아이템 이미지 그리기
-        screen.blit(box_surface, (equipped_item_rect.left - (box_size - equipped_item_rect.width) / 2,
-                            equipped_item_rect.top - (box_size - equipped_item_rect.height) / 2))
-        screen.blit(equipped_item, equipped_item_image_rect)  # 아이템 이미지 표시
-
-        # 아이템 종류에 따라 inventory_equipped_item 설정
-        if equipped_item == lab_state.inventory_wire:
-            lab_state.inventory_equipped_item = "lab_wire"
-        elif equipped_item == lab_state.inventory_switch:
-            lab_state.inventory_equipped_item = "lab_switch"
-        elif equipped_item == lab_state.keykard:
-            lab_state.inventory_equipped_item = "keykard"
-
-
 
     pygame.display.flip()
 
